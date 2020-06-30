4f2b5cedac86557127bdf4a590cb8e3559ab3a4d
fenocontreras <42077306+fenocontreras@users.noreply.github.com>
2020-06-30 19:23:22 -0400

    Update LoginActivity.java

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,19 +1,28 @@
 package com.kumiho.magicbox;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class LoginActivity extends AppCompatActivity {
 
     DatabaseHelper db;
     ImageView imgLogo;
     EditText etUsername, etPassword;
+    ImageView imgLogIn;
+    TextView tvSignUp;
     Button btnLogIn, btnSignIn;
 
     @Override
@@ -24,22 +33,34 @@ public class LoginActivity extends AppCompatActivity {
         db = new DatabaseHelper(this);
 
         imgLogo = (ImageView) findViewById(R.id.img_logo);
-        imgLogo.setImageResource(R.drawable.ic_launcher_background);
 
         etUsername = (EditText) findViewById(R.id.et_username);
         etPassword = (EditText) findViewById(R.id.et_password);
 
-        btnLogIn = (Button) findViewById(R.id.btn_log_in);
-        btnLogIn.setOnClickListener(new View.OnClickListener() {
+        imgLogIn = (ImageView) findViewById(R.id.img_log_in);
+        imgLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = etUsername.getText().toString().trim();
                 String password = etPassword.getText().toString().trim();
                 boolean res = db.checkUser(username, password);
                 if (res) {
-                    Toast.makeText(LoginActivity.this, "Successfully logged in.", Toast.LENGTH_SHORT).show();
+                    String extra = "a";
                     Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    intent.putExtra("USER_ID", db.getUserID(username));
+
+                    Cursor cursor = db.getUserData();
+
+                    if (cursor.moveToFirst()) {
+
+                        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                            if (cursor.getString(1).equals(username)) {
+                                extra = cursor.getString(0);
+                            }
+                        }
+                    }
+                    db.close();
+
+                    intent.putExtra("USER_ID", extra);
                     startActivity(intent);
                 }
                 else {
@@ -48,8 +69,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        btnSignIn = (Button) findViewById(R.id.btn_sign_in);
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        tvSignUp = (TextView) findViewById(R.id.tv_sign_up);
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent signInIntent = new Intent(LoginActivity.this, RegisterActivity.class);
